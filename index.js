25644ce762396e21cebb983a8ff99ff56321b820
natiqbaba232 <natiq4586@gmail.com>
2023-06-05 17:02:00 +0400

    cls

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-let marka = ["Mercedes", "BMW","AUDI","FORD","TOYOTA"];
-let Mercedes = ["CLS 63 AMG", "SLR","GLE 43 AMG"];
-let BMW = ["M5", "X5","530"];
-let AUDI = ["A6", "Q7","A4"];
-let FORD = ["Mustang", "Shelby gt500","F-150"];
-let TOYOTA = ["Camry", "Land Cruiser","Prius"];
+let marka = ["Mercedes", "BMW", "AUDI", "FORD", "TOYOTA"];
+let Mercedes = ["CLS 63 AMG", "SLR", "GLE 43 AMG"];
+let BMW = ["M5", "X5", "530"];
+let AUDI = ["A6", "Q7", "A4"];
+let FORD = ["Mustang", "Shelby gt500", "F-150"];
+let TOYOTA = ["Camry", "Land Cruiser", "Prius"];
 
 let sclt1 = document.getElementById('markaslct');
 let sclt2 = document.getElementById('modelslct');
 
-marka.forEach(function addMarka(item){
+marka.forEach(function addMarka(item) {
     let option = document.createElement("option");
-    option.text=item;
-    option.value=item;
+    option.text = item;
+    option.value = item;
     sclt1.appendChild(option);
 });
 
-sclt1.onchange = function(){
-    sclt2.innerHTML="<option></option";
-    if(this.value == "Mercedes"){
+sclt1.onchange = function () {
+    sclt2.innerHTML = "<option></option";
+    if (this.value == "Mercedes") {
         addToSclt2(Mercedes);
     }
-    if(this.value == "BMW"){
+    if (this.value == "BMW") {
         addToSclt2(BMW);
     }
-    if(this.value == "AUDI"){
+    if (this.value == "AUDI") {
         addToSclt2(AUDI);
     }
-    if(this.value == "FORD"){
+    if (this.value == "FORD") {
         addToSclt2(FORD);
     }
-    if(this.value == "TOYOTA"){
+    if (this.value == "TOYOTA") {
         addToSclt2(TOYOTA);
     }
 }
 
 
-function addToSclt2(arr){
-arr.forEach(function (item){
-    let option = document.createElement("option");
-    option.text = item;
-    option.value = item;
-    sclt2.appendChild(option);
-});
+function addToSclt2(arr) {
+    arr.forEach(function (item) {
+        let option = document.createElement("option");
+        option.text = item;
+        option.value = item;
+        sclt2.appendChild(option);
+    });
 }
 
 const boxes = [...document.querySelectorAll('.avto-box')]
@@ -66,32 +66,39 @@ form.addEventListener('submit', (e) => {
 
     const searchBox = []
     boxes.forEach(box => {
-        const qiymet = box.children[1].innerText.replaceAll(',','');
+        const qiymet = box.children[1].innerText.replaceAll(',', '');
         const modelMarkaAg = box.children[2].innerText;
+        const ag = parseInt(modelMarkaAg.split('(')[1])
         const yurusIl = box.children[3].innerText;
-        const yurus = parseInt(yurusIl.split(', ')[2].replaceAll(' ',''))
+        const yurus = parseInt(yurusIl.split(', ')[2].replaceAll(' ', ''))
         const il = parseInt(yurusIl.split(', ')[1])
+
         console.log(qiymet)
         const seher = box.children[4].innerText;
-        if(modelMarkaAg.includes(data.get('marka'))
-        && modelMarkaAg.includes(data.get('model'))
-        && qiymet.includes(data.get('valyuta'))
-        && seher.includes(data.get('seher'))
-        && (
-            (data.get('minqiymet') === '' && data.get('maxqiymet') === '')
-            || ((parseInt(data.get('minqiymet')) <= parseInt(qiymet) ) 
-            && (parseInt(data.get('maxqiymet')) >= parseInt(qiymet) ))
-        )
-        && (
-            (data.get('minyurus') === '' && data.get('maxyurus') === '')
-            || ((parseInt(data.get('minyurus')) <= yurus ) 
-            && (parseInt(data.get('maxyurus')) >= yurus ))
-        )
-        && (
-            (data.get('minil') === '' && data.get('maxil') === '')
-            || ((parseInt(data.get('minil')) <= il ) 
-            && (parseInt(data.get('maxil')) >= il ))
-        )) { 
+        if (modelMarkaAg.includes(data.get('marka'))
+            && modelMarkaAg.includes(data.get('model'))
+            && qiymet.includes(data.get('valyuta'))
+            && seher.includes(data.get('seher'))
+            && (
+                (data.get('minqiymet') === '' && data.get('maxqiymet') === '')
+                || ((parseInt(data.get('minqiymet')) <= parseInt(qiymet))
+                    && (parseInt(data.get('maxqiymet')) >= parseInt(qiymet)))
+            )
+            && (
+                (data.get('minyurus') === '' && data.get('maxyurus') === '')
+                || ((parseInt(data.get('minyurus')) <= yurus)
+                    && (parseInt(data.get('maxyurus')) >= yurus))
+            )
+            && (
+                (data.get('minguc') === '' && data.get('maxguc') === '')
+                || ((parseInt(data.get('minguc')) <= ag)
+                    && (parseInt(data.get('maxguc')) >= ag))
+            )
+            && (
+                (data.get('minil') === '' && data.get('maxil') === '')
+                || ((parseInt(data.get('minil')) <= il)
+                    && (parseInt(data.get('maxil')) >= il))
+            )) {
             searchBox.push(box);
         }
     })
@@ -114,15 +121,15 @@ document.querySelectorAll('.avto-btn').forEach(btn => {
         t.p2 = div.children[3].innerText
         t.p3 = div.children[4].innerText
         let find = false
-        for(let i=0; i<liked.length; i++) {
-            if(liked[i].img ===  t.img) {
+        for (let i = 0; i < liked.length; i++) {
+            if (liked[i].img === t.img) {
                 find = true;
                 break
             }
         }
-        if(!find) {
+        if (!find) {
             liked.push(t)
-            localStorage.setItem('liked',JSON.stringify(liked))
+            localStorage.setItem('liked', JSON.stringify(liked))
         }
     })
 })
